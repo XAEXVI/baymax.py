f40577c6afa812f0f70345e90e2e73e63861dc9a
Caio Reinelt <93232479+XAEXVI@users.noreply.github.com>
2022-02-10 18:32:07 -0300

    Update streaming.py

--- streaming.py
+++ streaming.py
@@ -1,61 +1,74 @@
-from re import I
 import requests
 import json
 import os
 import webbrowser
 
 def streaming():
-    
-    urln = 'https://www.netflix.com/browse'
-    urld = 'https://www.disneyplus.com/pt-br/home'
-    urlp = 'https://www.primevideo.com/?ref_=av_auth_return_redir&language=pt_br'
-    urlh = 'https://play.hbomax.com/page/urn:hbo:page:home/'
-    urlc = 'https://www.crunchyroll.com/pt-br'
-    urls = [ urld, urln, urlp, urlh]
+
+
+    services = ['Netflix','netflix','Disney+','disney+','Disney','disney','Disney +','disney +', 'Hbo Max', 'hbo Max', 'HboMax', 'hbomax', 'Hbomax', 'Hbomax','hbo','Hbo', 'Prime video', 'prime video', 'Prime', 'prime', 'Amazon Prime','amazon prime',' Crunchyroll', 'crunchyroll']
+    urln = 'https://www.netflix.com'
+    urld = 'https://www.disneyplus.com'
+    urlp = 'https://www.primevideo.com'
+    urlh = 'https://play.hbomax.com'
+    urlc = 'https://www.crunchyroll.com'
+    urlg = 'https://globoplay.globo.com/'
+    urlf = 'https://www.funimation.com'
+    urla = 'https://tv.apple.com'
+    urlst = 'https://www.starplus.com'
+    urls = [ urld, urln, urlp, urlh, urlc, urlg, urlf, urla]
     
     sr = str(input('Choose your Streaming service: '))
-    services = ['Netflix','netflix','tudum' ,'Tudum', 'Disney+','disney+','Disney','disney','Disney +','disney +', 'Hbo Max', 'hbo Max', 'HboMax', 'hbomax', 'Hbomax', 'Hbomax','hbo','Hbo', 'Prime video', 'prime video', 'Prime', 'prime', 'Amazon Prime','amazon prime',' Crunchyroll', 'crunchyroll']
-    quit = False
-    if sr.lower() in ['quit']:
-       quit = True
-        #hp ()
-    if  sr.lower() == "netflix": 
-            print('Opening the desired streaming service.')
-            webbrowser.open(urln)    
-    if sr.lower() in  ["disney", "disney+", "disney +"]:
-            print('Opening the desired streaming service.')
-            webbrowser.open(urld)           
-    if sr.lower() in  ["amazon prime", "prime", "prime video"]:
-            print('Opening the desired streaming service.')
-            webbrowser.open(urlp)
-    if sr.lower() in  ["hbo", "hbo max"]:
-            print('Opening the desired streaming service.')
-            webbrowser.open(urlh)
-    if sr.lower() in  ["Crunchy", "crunchyroll"]:
-            print('Opening the desired streaming service.')
-            webbrowser.open(urlc)
-
-    elif sr != services:
-        srr = str(input("It wasn't possible to recognize the desired streaming service, please try again: " ))
-        
-        quit = False
-        if srr.lower() in ['quit']:
+    if sr in ['quit']:
                 quit = True
-        
-        if  srr.lower() == "netflix": 
+                print('Welcome back to the main menu')
+    elif sr.lower() in ["netflix"]: 
                 print('Opening the desired streaming service.')
                 webbrowser.open(urln)    
-        if srr.lower() in  ["disney", "disney+", "disney +"]:
+    elif sr.lower() in  ["disney", "disney+", "disney +"]:
                 print('Opening the desired streaming service.')
                 webbrowser.open(urld)           
-        if srr.lower() in  ["amazon prime", "prime", "prime video"]:
+    elif sr.lower() in  ["amazon prime", "prime", "prime video"]:
                 print('Opening the desired streaming service.')
                 webbrowser.open(urlp)
-        if srr.lower() in  ["hbo", "hbo max"]:
+    elif sr.lower() in  ["hbo", "hbo max"]:
                 print('Opening the desired streaming service.')
-                webbrowser.open(urlh)           
-        if sr.lower() in  ["Crunchy", "crunchyroll"]:
-            print('Opening the desired streaming service.')
-            webbrowser.open(urlc)
+                webbrowser.open(urlh)
+    elif sr.lower() in  ["Crunchy", "crunchyroll"]:
+                print('Opening the desired streaming service.')
+                webbrowser.open(urlc)
+    elif sr.lower() in  ["globoplayer", "globo play", 'globo']:
+                print('Opening the desired streaming service.')
+                webbrowser.open(urlg)
+    elif sr.lower() in  ["funimation"]:
+                print('Opening the desired streaming service.')
+                webbrowser.open(urlf)
+    elif sr.lower() in  ["apple tv", "Apple"]:
+                print('Opening the desired streaming service.')
+                webbrowser.open(urla)
+    elif sr.lower() in  ["star +", "star+", 'star']:
+                print('Opening the desired streaming service.')
+                webbrowser.open(urlst)
+    else:
+        while sr.lower() not in services:
+                sr = input("It wasn't possible to recognize the desired streaming service Please try again: " )
+                quit = False
+                if sr.lower() in ['quit']:
+                        quit = True
+                elif sr.lower() in ["netflix", "Tudum"]: 
+                        print('Opening the desired streaming service.')
+                        webbrowser.open(urln)    
+                elif sr.lower() in  ["disney", "disney+", "disney +"]:
+                        print('Opening the desired streaming service.')
+                        webbrowser.open(urld)           
+                elif sr.lower() in  ["amazon prime", "prime", "prime video"]:
+                        print('Opening the desired streaming service.')
+                        webbrowser.open(urlp)
+                elif sr.lower() in  ["hbo", "hbo max"]:
+                        print('Opening the desired streaming service.')
+                        webbrowser.open(urlh)           
+                elif sr.lower() in  ["Crunchy", "crunchyroll"]:
+                        print('Opening the desired streaming service.')
+                        webbrowser.open(urlc)
+
 streaming()
-    
